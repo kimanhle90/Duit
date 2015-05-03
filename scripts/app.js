b7fffbf327f4ceed44bbe5bc5d76362d48134168
kimanhle90 <kimanh.le90@gmail.com>
2015-05-03 16:06:35 -0400

    removed ui-router

--- scripts/app.js
+++ scripts/app.js
@@ -2,22 +2,8 @@ var duitApp = angular.module("duitApp", ["ui.router", "firebase"]);
 
 duitApp.config(function($stateProvider, $urlRouterProvider) {
 
-	console.log("Angular is wokring");
+	console.log("Angular is working");
 
-	$urlRouterProvider.otherwise('/home');
-
-	$stateProvider
-        
-        // HOME VIEWS 
-        .state('home', {
-            url: '/',
-            templateUrl: '../assets/templates/home.html'
-        })
-        
-        // HISTORY VIEWS 
-        .state('history', {
-            url: '/history',
-            templateUrl: '../assets/templates/history.html'       
-        });
+	
 
 });	
