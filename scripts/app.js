cf8f2b6f7c4f204e8e3647c595c9832ce1630af2
kimanhle90 <kimanh.le90@gmail.com>
2015-05-05 18:19:47 -0400

    change ui-router method

--- scripts/app.js
+++ scripts/app.js
@@ -1,23 +1,41 @@
 var duitApp = angular.module("duitApp", ["ui.router", "firebase"]);
 
-duitApp.config(function($stateProvider, $urlRouterProvider) {
+// duitApp.config(function($stateProvider, $urlRouterProvider) {
 
-	console.log("Angular is working");
+// 	console.log("Angular is working");
 
-	$urlRouterProvider.otherwise('/home');
+// 	$urlRouterProvider.otherwise('/home');
 
-	$stateProvider
+// 	$stateProvider
         
-        // HOME VIEWS 
-        .state('home', {
-            url: '/',
-            templateUrl: '../assets/templates/home.html'
-        })
+//         // HOME VIEWS 
+//         .state('home', {
+//             url: '/',
+//             templateUrl: '../assets/templates/home.html'
+//         })
         
-        // HISTORY VIEWS 
-        .state('history', {
-            url: '/history',
-            templateUrl: '../assets/templates/history.html'       
-        });
+//         // HISTORY VIEWS 
+//         .state('history', {
+//             url: '/history',
+//             templateUrl: '../assets/templates/history.html'       
+//         });
 
-});	
+// });	
+
+duitApp.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
+   $locationProvider.html5Mode({
+    enabled: true,
+    requireBase: false
+    });
+ 
+   $stateProvider.state('history', {
+     url: '/history',
+     controller: 'history.controller',
+     templateUrl: '../assets/templates/history.html'
+   });
+ }]);
+
+duitApp.controller('history.controller', ['$scope', function($scope) {
+
+    }]);
+
