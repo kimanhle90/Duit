f7a681512a2a5629dcbc6ba9ee824b55c0e156f6
kimanhle90 <kimanh.le90@gmail.com>
2015-05-19 19:03:08 -0400

    Firebase working

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,7 @@
 (function(){
 
   // Start App Module
-var duitApp = angular.module("duitApp", ['ui.router', 'firebase']);
+  var duitApp = angular.module("duitApp", ['ui.router', 'firebase']);
 
 // This part control App Views
 
@@ -13,9 +13,9 @@ duitApp.config(['$stateProvider', '$locationProvider', function($stateProvider, 
 
  $stateProvider.state('home', {
   url: '/',
-  controller: 'home.controller',
+  controller: 'home.controller',  //Define as a controller
   templateUrl: '/assets/templates/home.html'
- });
+});
  
  $stateProvider.state('history', {
    url: '/history',
@@ -24,16 +24,51 @@ duitApp.config(['$stateProvider', '$locationProvider', function($stateProvider, 
  });
 }]);
 
-
-// This part triggers firebase data
-duitApp.controller("FirebaseCtrl", ["$scope", "$firebase", 
-  function ($scope, $firebase) {
-  var ref = new Firebase("https://duit.firebaseio.com/");
-
+duitApp.controller('home.controller',['$scope', function($scope) {
 }]);
+
+duitApp.controller('history.controller',['$scope', function($scope) {
+}]);
+
+
+// // This part triggers firebase data
+// duitApp.controller("FirebaseCtrl", ["$scope", "$firebaseArray",
+//   function($scope, $firebaseArray) {
+//     var ref = new Firebase("https://duit.firebaseio.com/tasks");
+//     $scope.tasks = $firebaseArray(ref);
+
+//   // add new items to the array
+//   // the message is automatically added to Firebase!
+//   $scope.addTask = function() {
+//     $scope.tasks.$add({
+//       info: $scope.newTaskInfo,
+//       priority: $scope.newTaskPriority,
+
+//     });
+//   };
+
+
+duitApp.controller("FirebaseCtrl", function($scope, $firebaseArray) {
+  var ref = new Firebase("https://duit.firebaseio.com/tasks");
+
+  $scope.tasks = $firebaseArray(ref);
+
+  $scope.addTask = function() {
+    $scope.tasks.$add({
+      text: $scope.newTaskText,
+    });
+  };
+
+});
+
+
 
 // End App Module
 
 })();
 
+// Add task input field/button
+// Set an attribute on task object to track completion status
+// In Angular view only show/hide tasks based on completion status
+// Create a condition that checks if date of object is >2 weeks from now
 
